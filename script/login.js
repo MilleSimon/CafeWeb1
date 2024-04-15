8c0207e38184892b521d3d5ec4202126a9f8a1d1
MilleSimon <isak.logenius@gmail.com>
2024-04-15 16:42:41 +0200

    login switching working

--- script/login.js
+++ script/login.js
@@ -1,1 +1,22 @@
-console.debug("hello")
+var loginDiv = document.getElementById("login");
+var locationDiv = document.getElementById("location");
+
+function loginSubmit()
+{
+    if (document.getElementById("loginUsername").value.length === 0)
+    {
+        loginDiv.getElementsByClassName("hiddenText")[0].removeAttribute("hidden");
+        return;
+    }
+
+    if (document.getElementById("loginPassword").value.length === 0)
+    {
+        loginDiv.getElementsByClassName("hiddenText")[0].removeAttribute("hidden");
+        return;
+    }
+
+    loginDiv.setAttribute("style", "Display: none;");
+    locationDiv.removeAttribute("style");
+    document.getElementById("loginTab").classList.remove("w3-red");
+    document.getElementById("locationTab").classList.add("w3-red");
+}
